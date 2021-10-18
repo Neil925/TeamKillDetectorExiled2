bf43e1e71d0705aa4c72052a0b3993c8e90d851e
Nao <60253860+NaoUnderscore@users.noreply.github.com>
2021-10-18 18:26:12 +0200

    things

--- TeamKillDetector/EventHandlers.cs
+++ TeamKillDetector/EventHandlers.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Globalization;
 using Exiled.Events.EventArgs;
 
@@ -6,10 +6,12 @@ namespace TeamKillDetector
 {
     internal class EventHandlers
     {
-        public void OnDied(DiedEventArgs ev)
+        internal void OnDied(DiedEventArgs ev)
         {
-            if (ev.Target.Side == ev.Killer.Side && ev.Killer != ev.Target)
-                ev.Target.Broadcast(Plugin.instance.Config.Time, Plugin.instance.Config.Message.Replace("{Player}", $"{ev.Killer.Nickname}{(Plugin.instance.Config.IncludeID ? " (" + ev.Killer.RawUserId + ")" : "")}").Replace("{Time}", DateTime.Now.ToString(CultureInfo.InvariantCulture)));
+            if (ev.Target.Side != ev.Killer.Side || ev.Killer == ev.Target)
+                return;
+            
+            ev.Target.Broadcast(Plugin.instance.Config.Time, Plugin.instance.Config.Message.Replace("{Player}", $"{ev.Killer.Nickname}{(Plugin.instance.Config.IncludeID ? " (" + ev.Killer.RawUserId + ")" : "")}").Replace("{Time}", DateTime.Now.ToString(CultureInfo.InvariantCulture)));
         }
     }
 }
